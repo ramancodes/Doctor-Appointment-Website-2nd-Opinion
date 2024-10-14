d6a8ba838495a59ddf449bac230d23c7e89f3d86
ramancodes <ramanmanjhi01@gmail.com>
2024-10-15 01:18:07 +0530

    Updated Some Text In the Front Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             {/* -----------------------Left Section---------------------- */}
             <div>
                 <img className='mb-5 w-40' src={assets.logo} alt="" />
-                <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p className='w-full md:w-2/3 text-gray-600 leading-6'>RM Care service enables patients to search for top doctors and book confirmed appointments. Whether it is a second opinion or a follow up question, why visit the doctor, when you can check with them online? </p>
             </div>
 
             {/* -----------------------Center Section---------------------- */}
@@ -33,7 +33,7 @@ const Footer = () => {
         </div>
         <div>
             {/* --------------------------Copyright text---------------- */}
-            <p className='py-5 text-sm text-center'>Copyright © 2024 Raman - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright © 2024, Raman - All Right Reserved.</p>
         </div>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             </p>
             <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
                 <img className='w-28' src={assets.group_profiles} alt="" />
-                <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block'/> schdule your appointment hassle-free.</p>
+                <p>Whether it is an appointment, a second opinion or a follow up <br className='hidden sm:block'/> question, simply browse through our extensive list of trusted <br className='hidden sm:block'/> doctors, schdule your appointment hassle-free.</p>
             </div>
             <a href='#speciality' className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
                 Book Appointment  <img className='w-3' src={assets.arrow_icon} alt="" />
